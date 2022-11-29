005117e1ba996a5192e500115ac6dc68a4b1abdc
evgeniy <shtorm20300@gmail.com>
2022-11-29 20:11:04 +0600

    ifelse

---
+++ Exemple_ifelse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username == "volk")
+{
+    Console.WriteLine("Привет, групый");
+}
+else
+{
+    Console.Write("Добро пожаловать, ");
+    Console.WriteLine(username);
+}
